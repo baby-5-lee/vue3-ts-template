1d67ab0db861236b481a4b03d5b750b84ffaf8a6
climber <944393179@qq.com>
2022-01-29 23:16:48 +0800

    vue3-template

---
+++ vite.config.ts
@@ -0,0 +1,25 @@
+import { resolve } from 'path'
+import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
+import AutoImport from 'unplugin-auto-import/vite'
+import Components from 'unplugin-vue-components/vite'
+import { ElementPlusResolver } from 'unplugin-vue-components/resolvers'
+
+export default defineConfig({
+  plugins: [
+    vue({
+      reactivityTransform: true
+    }),
+    AutoImport({
+      resolvers: [ElementPlusResolver()]
+    }),
+    Components({
+      resolvers: [ElementPlusResolver()]
+    })
+  ],
+  resolve: {
+    alias: {
+      '@': resolve(__dirname,'src')
+    }
+  }
+})
